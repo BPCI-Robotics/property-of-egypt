dc893ddb0bdf7c85b888e5d5b92f566d06d68529
Aseer Tayeem <aseertayeem@gmail.com>
2025-01-18 10:06:08 -0500

    install lemlib

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@ using namespace pros;
 Controller controller(CONTROLLER_MASTER);
 
 Motor liftIntake (7, MotorGears::blue, MotorUnits::counts);
-adi::Pneumatics left_pissed_in ('a', false, false);
-adi::Pneumatics right_pissed_in ('b', false, false);
+adi::Pneumatics left_piston ('a', false, false);
+adi::Pneumatics right_piston ('b', false, false);
 
 MotorGroup left_motors ({1, -2, -3}, v5::MotorGears::blue, v5::MotorUnits::counts);
 MotorGroup right_motors ({-4, 5, -6}, v5::MotorGears::blue, v5::MotorUnits::counts);
 
-v5::Vision color_sensor (9);
+v5::Vision vision_sensor (9);
 
 void initialize() {
     lcd::initialize();
@@ -21,8 +21,8 @@ void initialize() {
     static vision_signature_s_t BLUE_SIG = Vision::signature_from_utility (1, -3775, -3259, -3517, 4809, 7525, 6167, 3.0, 0);
     static vision_signature_s_t RED_SIG =  Vision::signature_from_utility (2,  7457,  9721,  8589, -611,    1, -305, 3.0, 0);
 
-    color_sensor.set_signature(1, &BLUE_SIG);
-    color_sensor.set_signature(2, &RED_SIG);
+    vision_sensor.set_signature(1, &BLUE_SIG);
+    vision_sensor.set_signature(2, &RED_SIG);
 }
 
 void disabled() {}
@@ -30,6 +30,7 @@ void disabled() {}
 void autonomous() {}
 
 void opcontrol() {
+
     int target_signature = 1;
     const int speed = 600;
 
@@ -57,12 +58,11 @@ void opcontrol() {
             liftIntake.move_velocity(0);
 
         if (controller.get_digital(DIGITAL_R1)) {
-            left_pissed_in.toggle();
-            right_pissed_in.toggle();
+            left_piston.toggle();
+            right_piston.toggle();
         }
 
-
-        vision_object_s_t obj = color_sensor.get_by_sig(0, target_signature);
+        vision_object obj = vision_sensor.get_by_sig(0, target_signature);
 
         delay(20);
     }
