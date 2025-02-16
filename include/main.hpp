458b4fa741469da450de5090c21a92784a9c022e
Aseer Tayeem <aseertayeem@gmail.com>
2025-02-15 22:49:27 -0500

    fix drivetrain declaration + superfluous changes

--- include/main.hpp
+++ include/main.hpp
@@ -7,31 +7,26 @@
 
 #include "autons.hpp"
 
-
-#ifdef __cplusplus
 extern "C" {
-#endif
-void autonomous(void);
-void initialize(void);
-void disabled(void);
-void competition_initialize(void);
-void opcontrol(void);
-#ifdef __cplusplus
+    void autonomous(void);
+    void initialize(void);
+    void disabled(void);
+    void competition_initialize(void);
+    void opcontrol(void);
 }
-#endif
-
-#ifdef __cplusplus
-// Add STL (standard template library) includes here if necessary (you probably don't need it.)
-#endif
 
 using namespace pros;
 
 inline Controller controller (pros::E_CONTROLLER_MASTER);
 
-inline MotorGroup left_motors ({1, -2, 3}, v5::MotorGears::blue, v5::MotorUnits::degrees);
-inline MotorGroup right_motors ({-4, 5, -6}, v5::MotorGears::blue, v5::MotorUnits::degrees);
+/* EZ template uses the first motor for tracking */
+inline ez::Drive chassis(
+    {1, -2, 3},
+    {-4, 5, -6},
+    10,  // IMU Port
 
-inline ez::Drive chassis ({1, -2, 3}, {-4, 5, -6}, 10, 4.0, 600.0, 600.0 / 360.0);
+    3.375,   // Wheel Diameter (3.25 + 0.125)
+    360.0);  // Wheel RPM
 
 inline Motor lift_intake (-7, v5::MotorGears::blue, v5::MotorUnits::degrees);
 inline Motor wall_stake (8, v5::MotorGears::red, v5::MotorUnits::degrees); 
